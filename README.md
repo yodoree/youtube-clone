ce110b9de4bd11558d53e9024a2221bfe28d6e6f
yodoree <yodoree@gmail.com>
2021-12-29 15:26:19 +0900

    Create Github Login

---
+++ README.md
@@ -0,0 +1,16 @@
+# Youtube clone
+
+/ -> home
+/join -> Join 회원가입
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+/users/edit -> Edit My Profile
+/users/logout -> Log Out
+/users/delete -> Delete My Profile
+
+/videos/:id -> Watch video
+/videos/:id/edit -> Delete edit
+/videos/:id/delete -> Delete video
+/videos/upload -> Upload video
---
+++ src/routers/userRouter.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  edit,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController";
+const userRouter = express.Router();
+
+userRouter.get("/logout", logout);
+userRouter.get("/edit", edit);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get(":id", see);
+
+export default userRouter;
