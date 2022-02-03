5829531eb622f5beba7e7cb98ee0c8f16fb052c7
yodoree <yodoree@gmail.com>
2022-02-03 11:17:31 +0900

    feat: Create flash message
    에러메세지를 유저에게 아름답게 보여드림

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -45,6 +45,7 @@ export const postEdit = async (req, res) => {
     description,
     hashtags: Video.formatHashtags(hashtags),
   });
+  req.flash("success", "Changes saved.");
   return res.redirect(`/videos/${id}`);
 };
 
@@ -91,6 +92,7 @@ export const deleteVideo = async (req, res) => {
     return res.render("404", { pageTitle: "Video not found." });
   }
   if (String(video.owner) !== String(_id)) {
+    req.flash("error", "You are not the owner of the video");
     return res.status(403).redirect("/");
   }
   await Video.findByIdAndDelete(id);
